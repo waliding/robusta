9883d04fb0a17c927be0cc94c4a1d03cfd3dc754
luidnel-maignan <57224909+luidnel-maignan@users.noreply.github.com>
2019-11-14 18:08:27 +0100

    Remove "compile" action and ".jvs" checking

--- src/main/java/org/robusta/Main.java
+++ src/main/java/org/robusta/Main.java
@@ -4,38 +4,14 @@ import org.robusta.compiler.JvsCompiler;
 
 public class Main {
 
-	private final static String COMPILE_GOAL = "compile";
 	public final static long COMPILATION_START_TIME = System.currentTimeMillis();
 	
 	public static void main(String[] args) {
-		if (args.length == 2) {
-			handleActions(args);
+		if (args.length != 1) {
+			System.out.println("Syntaxe error, command should be : java -jar robusta.jar file_path");
 		} else {
-			System.out.println("Syntaxe error, command should be : java -jar robusta.jar compile file_path");
+			JvsCompiler.build(args[0]);
 		}
-	}
-
-	private static void handleActions(String[] args) {
-		String goal = args[0];
-		switch (goal) {
-		case COMPILE_GOAL:
-			String javaFilePath = args[1];
-			if (verifyExtention(javaFilePath)) {
-				JvsCompiler.build(javaFilePath);
-			} else {
-				System.out.println("The file should have .jvs extension");
-			}
-
-			break;
-		}
-		
-	}
-
-
-	public static boolean verifyExtention(String path) {
-		// TODO : verify extension efficiently.
-		return path.contains(".jvs");
-
 	}
 
 }
